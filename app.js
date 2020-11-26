2fcc30dbf0342b5c293ced69614036dc5cadb5f4
Pedro Donda <phfdonda@gmail.com>
2020-11-26 13:28:37 -0300

    Initial files

---
+++ app.js
@@ -0,0 +1,84 @@
+// CONFIG  =====================================================================
+const express = require('express')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+const mongoose = require('mongoose')
+const app = express()
+const serverURL = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.znb2f.mongodb.net/secretsDB"
+const mongooseSetting = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}
+
+mongoose
+  .connect(serverURL, mongooseSetting)
+  .then(console.log('Connected successfully to the DB! =D'))
+  .catch("Oops! No connection to the DB! ='C")
+
+app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+// =============================================================================
+
+// GLOBAL VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const validPaths = ['home','login','register','secrets','submit']
+const userSchema = {
+  email: String,
+  password: String
+}
+const User = mongoose.model("User", userSchema)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// GET HOME <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  app.get('/',(req,res)=>{
+    res.render('home')
+  })
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// GET PATH <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  app.get('/:path',(req,res)=>{
+    const path = req.params.path
+    validPaths.includes(path) ? res.render(path) : res.render('lost')
+  })
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// POST REGISTER >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.post('/register', (req,res)=>{
+   User.create({
+     email: req.body.username,
+     password: req.body.password
+   }, err =>{
+     err ? res.send(err) :
+      res.render('secrets')
+   })
+})
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// POST REGISTER >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.post('/login', (req,res)=>{
+   User.findOne({
+     email: req.body.username,
+   }, (err, foundUser) =>{
+     if(err){res.send(err)} else
+      if(foundUser){
+        if(foundUser.password === req.body.password){
+          res.render('secrets')
+        }else{
+          res.send('Wrong Password!')
+        }
+      }else{
+        res.send('No user registered with this email, try to Register')
+      }
+   })
+})
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// APP LISTEN ??????????????????????????????????????????????????????????????????
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log('Server started at port 3000')
+})
+// ?????????????????????????????????????????????????????????????????????????????
